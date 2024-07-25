2e620ea76beb21ccc7fca91d405a3018c3b0e666
cinothejk <marco.diblasi@external.stellantis.com>
2024-07-25 17:24:53 +0200

    Spring H2 DB JDBC

---
+++ SpringBootTutorialH2DatabaseJDBC/src/main/java/com/cino/SpringBootTutorialH2DatabaseJDBC/SpringBootTutorialH2DatabaseJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.cino.SpringBootTutorialH2DatabaseJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialH2DatabaseJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorialH2DatabaseJdbcApplication.class, args);
+	}
+
+}
---
+++ SpringBootTutorialH2DatabaseJDBC/src/test/java/com/cino/SpringBootTutorialH2DatabaseJDBC/SpringBootTutorialH2DatabaseJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cino.SpringBootTutorialH2DatabaseJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialH2DatabaseJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
