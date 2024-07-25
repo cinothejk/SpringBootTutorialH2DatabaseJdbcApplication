a19a87c3c6a1259bac46593e8e29621193486167
cinothejk <marco.diblasi@external.stellantis.com>
2024-07-25 17:48:55 +0200

    @Repository era Mancante

--- SpringBootTutorialH2DatabaseJDBC/src/main/java/com/cino/SpringBootTutorialH2DatabaseJDBC/repository/UserRepository.java
+++ SpringBootTutorialH2DatabaseJDBC/src/main/java/com/cino/SpringBootTutorialH2DatabaseJDBC/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.cino.SpringBootTutorialH2DatabaseJDBC.repository;
 
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cino.SpringBootTutorialH2DatabaseJDBC.model.Users;
 
+@Repository
 public interface UserRepository extends ListCrudRepository<Users, Integer> {
 
 }
